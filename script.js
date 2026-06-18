08be8c65f1313a53d88b3a7b73a067b050fd3141
jackforce <radwandoufan@gmail.com>
2026-06-18 16:39:43 +0200

    removed the disc pic from the tracklist, started adding js logic by adding variables for the currentTrackIndex, audio player and some functions that are still not 100% functional need to figure out how if statements syntax work cause its not like python

--- script.js
+++ script.js
@@ -1,105 +1,119 @@
 const tracklist = [
   {
     name: "Sweden (C418)",
-    audioSrc: "audio/c418_sweden.mp3",
-    discImageSrc: "images/disc_sweden_exposed.png" // The green record (Background track)
+    audioSrc: "audio/c418_sweden.mp3"
   },
   {
     name: "Cat (C418)",
-    audioSrc: "audio/c418_cat.mp3",
-    discImageSrc: "images/disc_cat_exposed.png" // Green center
+    audioSrc: "audio/c418_cat.mp3"
   },
   {
     name: "13 (C418)",
-    audioSrc: "audio/c418_13.mp3",
-    discImageSrc: "images/disc_13_exposed.png" // White and yellow center
+    audioSrc: "audio/c418_13.mp3"
   },
   {
     name: "Blocks (C418)",
-    audioSrc: "audio/c418_blocks.mp3",
-    discImageSrc: "images/disc_blocks_exposed.png" // Red center
+    audioSrc: "audio/c418_blocks.mp3"
   },
   {
     name: "Chirp (C418)",
-    audioSrc: "audio/c418_chirp.mp3",
-    discImageSrc: "images/disc_chirp_exposed.png" // Dark red/maroon center
+    audioSrc: "audio/c418_chirp.mp3"
   },
   {
     name: "Far (C418)",
-    audioSrc: "audio/c418_far.mp3",
-    discImageSrc: "images/disc_far_exposed.png" // Light green center
+    audioSrc: "audio/c418_far.mp3"
   },
   {
     name: "Mall (C418)",
-    audioSrc: "audio/c418_mall.mp3",
-    discImageSrc: "images/disc_mall_exposed.png" // Dark blue center
+    audioSrc: "audio/c418_mall.mp3"
   },
   {
     name: "Mellohi (C418)",
-    audioSrc: "audio/c418_mellohi.mp3",
-    discImageSrc: "images/disc_mellohi_exposed.png" // Purple and white borders
+    audioSrc: "audio/c418_mellohi.mp3"
   },
   {
     name: "Stal (C418)",
-    audioSrc: "audio/c418_stal.mp3",
-    discImageSrc: "images/disc_stal_exposed.png" // Black/Dark grey center
+    audioSrc: "audio/c418_stal.mp3"
   },
   {
     name: "Strad (C418)",
-    audioSrc: "audio/c418_strad.mp3",
-    discImageSrc: "images/disc_strad_exposed.png" // White center
+    audioSrc: "audio/c418_strad.mp3"
   },
   {
     name: "Ward (C418)",
-    audioSrc: "audio/c418_ward.mp3",
-    discImageSrc: "images/disc_ward_exposed.png" // Dark green and mint borders
+    audioSrc: "audio/c418_ward.mp3"
   },
   {
     name: "11 (C418)",
-    audioSrc: "audio/c418_11.mp3",
-    discImageSrc: "images/disc_11_exposed.png" // Cracked/shattered black disc
+    audioSrc: "audio/c418_11.mp3"
   },
   {
     name: "Wait (C418)",
-    audioSrc: "audio/c418_wait.mp3",
-    discImageSrc: "images/disc_wait_exposed.png" // Sky blue center
+    audioSrc: "audio/c418_wait.mp3"
   },
   {
     name: "Pigstep (Lena Raine)",
-    audioSrc: "audio/lena_raine_pigstep.mp3",
-    discImageSrc: "images/disc_pigstep_exposed.png" // Full red colored vinyl
+    audioSrc: "audio/lena_raine_pigstep.mp3"
   },
   {
     name: "Otherside (Lena Raine)",
-    audioSrc: "audio/lena_raine_otherside.mp3",
-    discImageSrc: "images/disc_otherside_exposed.png" // Deep blue vinyl with a green border
+    audioSrc: "audio/lena_raine_otherside.mp3"
   },
   {
     name: "5 (Samuel Åberg)",
-    audioSrc: "audio/samuel_aberg_5.mp3",
-    discImageSrc: "images/disc_5_exposed.png" // Reassembled dark grey fragments
+    audioSrc: "audio/samuel_aberg_5.mp3"
   },
   {
     name: "Relic (Aaron Cherof)",
-    audioSrc: "audio/aaron_cherof_relic.mp3",
-    discImageSrc: "images/disc_relic_exposed.png" // Dark brown and turquoise vinyl
+    audioSrc: "audio/aaron_cherof_relic.mp3"
   },
   {
     name: "Precipice (Aaron Cherof)",
-    audioSrc: "audio/aaron_cherof_precipice.mp3",
-    discImageSrc: "images/disc_precipice_exposed.png" // Multi-colored grey, green, and orange border
+    audioSrc: "audio/aaron_cherof_precipice.mp3"
   },
   {
     name: "Creator (Lena Raine)",
-    audioSrc: "audio/lena_raine_creator.mp3",
-    discImageSrc: "images/disc_creator_exposed.png" // Vibrant full green vinyl
+    audioSrc: "audio/lena_raine_creator.mp3"
   },
   {
     name: "Creator - Music Box Version (Lena Raine)",
-    audioSrc: "audio/lena_raine_creator_music_box.mp3",
-    discImageSrc: "images/disc_creator_music_box_exposed.png" // Full orange colored vinyl
+    audioSrc: "audio/lena_raine_creator_music_box.mp3"
   }
 ];
+
+
 let currentTrackIndex = 0;
 let isPlaying = false;
-document.getElementById()
+
+const prevbutton = document.getElementById("prev-button");
+const playbutton = document.getElementById("play-button");
+const nextbutton = document.getElementById("next-button");
+const trackTitleDisplay = document.querySelector(".now-playing");
+const audioPlayer = document.querySelector("audio");
+
+function load_track(index) {
+  const currentTrack = tracklist[index];
+  audioPlayer.src = currentTrack.audioSrc;
+  trackTitleDisplay.name = "Now Playing: " + currentTrack.name;
+}
+function togglePlay() {
+  
+}
+function togglePause() {
+
+}
+function nextSong() {
+  if currentTrackIndex == 19:
+    currentTrackIndex = 0
+  else:
+    currentTrackIndex += 1
+  load_track(currentTrackIndex);
+}
+function prevSong() {
+  if currentTrackIndex == 0:
+    currentTrack = 19;
+  else:
+    currentTrackIndex -= 1;
+  
+  load_track(currentTrackIndex);
+}
